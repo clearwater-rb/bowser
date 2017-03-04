4ad35e003babd386b8723a73d471ed5b89245b7f
Jamie Gaskins <jgaskins@gmail.com>
2017-03-04 00:49:42 -0500

    Add DelegateNative module (#19)
    * Add DelegateNative module
    
    This commit extracts the method_missing functionality from Element to
    its own module.
    
    This module can be included to delegate unhandled incoming messages to
    the native object being wrapped. These messages will be transformed to
    match JavaScript conventions and there may be several properties checked
    for a single message.
    
    Spec coverage still lives in element_spec.rb. We may want to extract
    that, as well, but it's okay to remain there for now.
    
    * Add include_all parameter in `respond_to_missing?`
    
    The include_all flag determines whether to include protected and private
    methods, but there is no such thing in JavaScript, so we just ignore
    that argument, though we accept it for the purpose of arity.

---
+++ opal/bowser/delegate_native.rb
@@ -0,0 +1,54 @@
+module Bowser
+  module DelegateNative
+    # Fall back to native properties.
+    def method_missing message, *args, &block
+      property_name = property_for_message(message)
+      property = `#@native[#{property_name}]`
+
+      # translate setting a property
+      if message.end_with? '='
+        return `#@native[#{property_name}] = args[0]`
+      end
+
+      # If the native element doesn't have this property, bubble it up
+      super if `#{property} === undefined`
+
+      if `property === false`
+        return false
+      else
+        property = `property == null ? nil : property`
+      end
+
+      # If it's a method, call it. Otherwise, return it.
+      if `typeof(property) === 'function'`
+        `property.apply(#@native, args)`
+      else
+        property
+      end
+    end
+
+    def respond_to_missing? message, include_all
+      return true if message.end_with? '='
+      return true if property_for_message(message)
+
+      false
+    end
+
+    def property_for_message message
+      camel_cased_message = message
+        .gsub(/_\w/) { |match| `match[1]`.upcase }
+        .sub(/=$/, '')
+
+      # translate `supported?` to `supported` or `isSupported`
+      if message.end_with? '?'
+        camel_cased_message = camel_cased_message.chop
+        property_type = `typeof(#@native[camel_cased_message])`
+        if property_type == 'undefined'
+          camel_cased_message = "is#{camel_cased_message[0].upcase}#{camel_cased_message[1..-1]}"
+        end
+      end
+
+      camel_cased_message
+    end
+  end
+end
--- opal/bowser/element.rb
+++ opal/bowser/element.rb
@@ -1,9 +1,11 @@
 require 'bowser/event_target'
 require 'bowser/file_list'
+require 'bowser/delegate_native'
 
 module Bowser
   class Element
     include EventTarget
+    include DelegateNative
 
     def initialize native
       @native = native
@@ -76,45 +78,6 @@ module Bowser
 
     def files
       FileList.new(`#@native.files`)
-    end
-
-    # Fall back to native properties.
-    def method_missing message, *args, &block
-      camel_cased_message = message
-        .gsub(/_\w/) { |match| `match[1]`.upcase }
-        .sub(/=$/, '')
-
-      # translate setting a property
-      if message.end_with? '='
-        return `#@native[camel_cased_message] = args[0]`
-      end
-
-      # translate `supported?` to `supported` or `isSupported`
-      if message.end_with? '?'
-        camel_cased_message = camel_cased_message.chop
-        property_type = `typeof(#@native[camel_cased_message])`
-        if property_type == 'undefined'
-          camel_cased_message = "is#{camel_cased_message[0].upcase}#{camel_cased_message[1..-1]}"
-        end
-      end
-
-      # If the native element doesn't have this property, bubble it up
-      super if `typeof(#@native[camel_cased_message]) === 'undefined'`
-
-      property = `#@native[camel_cased_message]`
-
-      if `property === false`
-        return false
-      else
-        property = `property == null ? nil : property`
-      end
-
-      # If it's a method, call it. Otherwise, return it.
-      if `typeof(property) === 'function'`
-        `property.apply(#@native, args)`
-      else
-        property
-      end
     end
 
     def ==(other)
