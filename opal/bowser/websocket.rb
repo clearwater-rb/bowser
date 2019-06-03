fc531f4c9d82f62c3031f602e317c2f62ca80e4a
Jamie Gaskins <jgaskins@gmail.com>
2019-06-02 22:13:15 -0400

    Reset event handlers *after* closing
    We still need to run the `close` event handler

--- opal/bowser/websocket.rb
+++ opal/bowser/websocket.rb
@@ -65,8 +65,8 @@ module Bowser
     # @param reason [String, nil] the reason this socket is being closed
     def close reason=`undefined`
       @autoreconnect = false
-      @handlers = {}
       `#@native.close(reason)`
+      @handlers = Hash.new { |h, k| h[k] = [] }
     end
 
     private
